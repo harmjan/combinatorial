05647a3d04c7ec576482be6140e2cacdf882d0f5
Harmjan Treep <mail@harmjantreep.nl>
2013-11-26 13:16:07 +0100

    Add a problem generator.
    Problems can be generated, the N and K of the problem to
    generate can be passed over cin.
    
    On linux can you run:
    	echo "30 15" | ./generateProblem/generate | ./naive/naive
    for example to test the naive solver on a problem with
    N=30 and K=15.

---
+++ generateProblem/generate.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#include <vector>
+
+std::vector<std::pair<std::vector<int>,std::vector<double>>> subfunctions;
+
+int main() {
+	int n, k;
+	std::cin >> n >> k;
+
+	srand(time(NULL));
+
+	// Generate the start bitstring
+	std::vector<bool> bitstring(n);
+	for( int i=0; i<n; ++i ) {
+		bitstring[i] = rand()%2==0;
+	}
+
+	// Generate the subfunctions
+	subfunctions.resize(n);
+	for( int i=0; i<n; ++i ) {
+		// While generating the dependencies make sure you don't generate
+		// the same dependency twice per subfunction. Also make sure that
+		// the first dependency is the bitnumber so every bit exists at
+		// least in 1 subfunction.
+		subfunctions[i].first.resize(k);
+		if( k > 0 )
+			subfunctions[i].first[0] = i;
+		for( int j=1; j<k; ++j ) {
+			int new_dependency;
+			bool unique;
+			do {
+				unique = true;
+				new_dependency = rand() % n;
+				for( int k=0; k<j; ++k ) {
+					if( subfunctions[i].first[k] == new_dependency ) {
+						unique = false;
+						break;
+					}
+				}
+			} while(!unique);
+			subfunctions[i].first[j] = new_dependency;
+		}
+
+		// Generate the fitness values
+		subfunctions[i].second.resize(1<<k);
+		for( int j=0; j<(1<<k); ++j ) {
+			subfunctions[i].second[j] = static_cast<double>(rand()) / static_cast<double>(rand());
+		}
+	}
+
+	// Output the generated instance
+	std::cout << n << " " << k << std::endl;
+	for( bool bit : bitstring )
+		std::cout << bit << " ";
+	std::cout << std::endl;
+	for( auto subfunc : subfunctions ) {
+		for( int depend : subfunc.first )
+			std::cout << depend << " ";
+		std::cout << std::endl;
+		for( double fitness : subfunc.second )
+			std::cout << fitness << " ";
+		std::cout << std::endl;
+	}
+}
